a0c58994eb13c89db0cdf6f9c608957a38b8a890
antimatter15 <antimatter15@1c2e5cc6-2bb9-11de-ba44-d58fe8a9ce33>
2009-04-18 02:05:50 +0000

    php files

---
+++ list.php
@@ -0,0 +1,14 @@
+<?php
+header("content-type:text/plain");
+
+foreach(explode("\n", file_get_contents("list.txt")) as $line){
+  echo ":".$line."\n";
+  $files = scandir("list/$line");
+  foreach($files as $file){
+    if($file != '.' && $file != '..'){
+      echo $line,':',$file, "\n";
+    }
+  }
+}
+s
+?>
