d70b1a11c957e7c6959af304abdf09a058e2a075
antimatter15 <antimatter15@1c2e5cc6-2bb9-11de-ba44-d58fe8a9ce33>
2009-04-18 02:10:55 +0000

    more damlevyness

--- trunk/lib.js
+++ trunk/lib.js
@@ -2,6 +2,9 @@
 //based on: http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Levenshtein_distance
 //and:  http://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
 function damlev( str1, str2 ){
+  if(!str1)str1="";
+  if(!str2)str2="";
+
 	var i, j, cost, d = [];
   str1 = str1.split("");
   str2 = str2.split("");
@@ -37,10 +40,29 @@ function damlev( str1, str2 ){
 	return d[str1.length][str2.length];
 }
 
+/*
+Script by RoBorg
+RoBorg@geniusbug.com
+http://javascript.geniusbug.com | http://www.roborg.co.uk
+Please do not remove or edit this message
+Please link to this website if you use this script!
+*/
 
+function clone(myObj)
+{
+	if(typeof(myObj) != 'object') return myObj;
+	if(myObj == null) return myObj;
 
+	var myNewObj = new Object();
 
+	for(var i in myObj)
+		myNewObj[i] = clone(myObj[i]);
 
+	return myNewObj;
+}
+
+//from my awesome vX JS Library, http://vxjs.googlecode.com
 var _=_?_:{}
 _.tpl=_.M=function(t,d,x){for(x in d)t=t.split("{"+x+"}").join(d[x]);return t}
 _.extend=_.T=function(o,a,y){for(y in a)o[y]=a[y];return o}
+_.json=_.S=function(j,d,t){if(d)return eval('('+j+')');if(!j)return j+'';t=[];if(j.pop){for(x in j)t.push(_.S(j[x]));j='['+t.join(',')+']'}else if(typeof j=='object'){for(x in j)t.push(x+':'+_.S(j[x]));j='{'+t.join(',')+'}'}else if(j.split)j="'"+j.replace(/\'/g,"\\'")+"'";return j}
