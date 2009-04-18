cfc94c74a7268ca02037e9531dd28abf153ca284
antimatter15 <antimatter15@1c2e5cc6-2bb9-11de-ba44-d58fe8a9ce33>
2009-04-18 02:12:06 +0000

    for some reason # symbols get awkwardly escaped so i cant directly ajax the direcotry

---
+++ trunk/proxy.php
@@ -0,0 +1,12 @@
+<?php
+  $fn = $_REQUEST['fn'];
+  if($fn && strpos($fn, "..") === FALSE){
+    if(file_exists("list/$fn")){
+      echo file_get_contents("list/$fn");
+    }else{
+      echo "ERROR:NO FILE";
+    }
+  }else{
+    echo "ERROR:INVALID FILENAME";
+  }
+?>
