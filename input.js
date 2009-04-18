f3c489fd3aec6b4b2aca4f36204dffbdc5b45b8c
antimatter15 <antimatter15@1c2e5cc6-2bb9-11de-ba44-d58fe8a9ce33>
2009-04-18 02:10:43 +0000

    the command handling structure

--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 var commands = {};
+var cmdmode = false;
 
 $(document).ready(function(){
   $("#inputform").submit(function(e){
@@ -10,23 +11,35 @@ $(document).ready(function(){
   })
 })
 
+
 function handle_input(args){
+  if(cmdmode&&("<?!:.$>".indexOf(args.substr(0,1))<0||args==""))args="."+args;//hacquetyhaqhak
   if(args.substr(0,3) == "js:"){
     try{
-      msg(eval(args.substr(3)),"gray");
+      msg(_.json(eval(args.substr(3))),"gray");
     }catch(err){
-      msg("Execution Error:","red");
+      msg("Execution Error: "+err,"red");
     }
-  }else if("<?!:".indexOf(args.substr(0,1)) != -1){
-    var parts = args.substr(1).split(/\s|;|:/)
-    if(commands[parts[0]]){
+  }else if("<?!:.$>".indexOf(args.substr(0,1)) != -1 && args != ""){
+    if(args.split(/\s|:|;/).length > 0){
+      var cmd = $.trim(args.substr(1, args.split(/\s|:|;/)[0].length-1));
+    }else{
+      var cmd = "";
+    }
+    var parts = $.map(args.substr(cmd.length+2).replace(/\\,/g,"PENGUINZRKUTE").split(","),function(e){
+      return $.trim(e).replace(/PENGUINZRKUTE/g, ",")})
+    //var parts = args.substr(1).substr(0,args.indexOf(/\s|:|;/)//args.substr(1).split(/\s|;|:/)
+    if(args.substr(1) == ""){
+      cmd = "_blank"
+    }
+    if(commands[cmd]){
       try{
-        msg(commands[parts[0]](parts[1]),"gray");
+        msg(commands[cmd].apply(this, parts), "gray");
       }catch(err){
-        msg("Command Execution Error:","red");
+        msg("Execution Error ("+cmd+"): "+err, "red");
       }
     }else{
-      msg("Command Not Found", "red");
+      msg("Command Not Found: "+cmd, "red");
     }
     //find command
     
