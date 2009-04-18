f105208eeba0eb9cdecc1bf77af01cb5d323bf5b
antimatter15 <antimatter15@1c2e5cc6-2bb9-11de-ba44-d58fe8a9ce33>
2009-04-18 02:11:21 +0000

    the server stuffs

--- trunk/list.php
+++ trunk/list.php
@@ -2,13 +2,14 @@
 header("content-type:text/plain");
 
 foreach(explode("\n", file_get_contents("list.txt")) as $line){
-  echo ":".$line."\n";
-  $files = scandir("list/$line");
-  foreach($files as $file){
-    if($file != '.' && $file != '..'){
-      echo $line,':',$file, "\n";
+  if($line){
+    echo ":".$line."\n";
+    $files = scandir("list/$line");
+    foreach($files as $file){
+      if($file != '.' && $file != '..'){
+        echo $line,':',$file, "\n";
+      }
     }
   }
 }
-s
 ?>
